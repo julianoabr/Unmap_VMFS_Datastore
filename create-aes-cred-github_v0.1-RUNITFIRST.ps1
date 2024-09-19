817bbf4dbf221f935693232a0b86bb6e2679f210
Juliano Ribeiro <35538913+julianoabr@users.noreply.github.com>
2024-09-18 21:12:17 -0300

    Update create-aes-cred-github_v0.1-RUNITFIRST.ps1

--- create-aes-cred-github_v0.1-RUNITFIRST.ps1
+++ create-aes-cred-github_v0.1-RUNITFIRST.ps1
@@ -6,15 +6,15 @@
 #1. Create KEY
 $Key = New-Object Byte[] 32
 [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
-$Key | Out-File -FilePath "$env:SystemDRive\Encrypt\aes.key"
+$Key | Out-File -FilePath "$env:SystemDrive\Encrypt\aes.key"
 
 
 #2. Create Credential using AES KEY
-(get-credential).Password | ConvertFrom-SecureString -key (Get-Content "$env:SystemDRive\Encrypt\aes.key") | Set-Content "$env:SystemDRive\Encrypt\domain-password.txt"
+(get-credential).Password | ConvertFrom-SecureString -key (Get-Content "$env:SystemDrive\Encrypt\aes.key") | Set-Content "$env:SystemDrive\Encrypt\domain-password.txt"
 
 
 
 #3.Using Password with Script
 $userName = 'domain\user'
-$password = Get-Content "$env:SystemDRive\Encrypt\domain-password.txt" | ConvertTo-SecureString -Key (Get-Content "$env:SystemDRive\Encrypt\aes.key")
+$password = Get-Content "$env:SystemDrive\Encrypt\domain-password.txt" | ConvertTo-SecureString -Key (Get-Content "$env:SystemDrive\Encrypt\aes.key")
 $credential = New-Object System.Management.Automation.PsCredential($username,$password)
