302c5af95c80dabfaca1192786bf08d6f5a2400c
Juliano Ribeiro <35538913+julianoabr@users.noreply.github.com>
2024-09-18 21:11:22 -0300

    Add files via upload

---
+++ create-aes-cred-github_v0.1-RUNITFIRST.ps1
@@ -0,0 +1,20 @@
+﻿#BEFORE USING MAIN SCRIPT YOU HAVE TO RUN THIS TO CREATE
+#1. AES.KEY
+#2. SAVE YOUR DOMAIN CRED
+#3. CREATE CRED TO USE IN YOUR SCRIPT
+
+#1. Create KEY
+$Key = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Key | Out-File -FilePath "$env:SystemDRive\Encrypt\aes.key"
+
+
+#2. Create Credential using AES KEY
+(get-credential).Password | ConvertFrom-SecureString -key (Get-Content "$env:SystemDRive\Encrypt\aes.key") | Set-Content "$env:SystemDRive\Encrypt\domain-password.txt"
+
+
+
+#3.Using Password with Script
+$userName = 'domain\user'
+$password = Get-Content "$env:SystemDRive\Encrypt\domain-password.txt" | ConvertTo-SecureString -Key (Get-Content "$env:SystemDRive\Encrypt\aes.key")
+$credential = New-Object System.Management.Automation.PsCredential($username,$password)
